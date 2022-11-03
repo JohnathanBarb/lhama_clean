895a498a2f5f9beb323c3f8eac41a182cf417952
Johnathan Barbosa <jbarbosa0504@gmail.com>
2022-11-02 22:21:42 -0300

    feat: implementing select_users in UserRepository

--- src/infra/repo/user_repository.py
+++ src/infra/repo/user_repository.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from src.infra.config import DBConnectionHandler
 from src.infra.entities import Users as UsersModel
 from src.domain.models import Users
@@ -28,3 +30,44 @@ class UserRepository:
                 raise
             finally:
                 db_connection.session.close()
+
+    @classmethod
+    def select_user(cls, user_id: int = None, name: str = None) -> List[Users]:
+        """Select data in user entity by id and/or name
+        :param - user_id: ID of the registry
+               - name: user name
+        :return - List with Users selected
+        """
+        try:
+            query_data = None
+
+            if user_id and not name:
+                with DBConnectionHandler() as db_connection:
+                    data = (
+                        db_connection.session.query(UsersModel)
+                        .filter_by(id=user_id)
+                        .one()
+                    )
+                    query_data = [data]
+            elif not user_id and name:
+                with DBConnectionHandler() as db_connection:
+                    data = (
+                        db_connection.session.query(UsersModel)
+                        .filter_by(name=name)
+                        .one()
+                    )
+                    query_data = [data]
+            elif user_id and name:
+                with DBConnectionHandler() as db_connection:
+                    data = (
+                        db_connection.session.query(UsersModel)
+                        .filter_by(id=user_id, name=name)
+                        .one()
+                    )
+                    query_data = [data]
+            return query_data
+        except:
+            db_connection.session.rollback()
+            raise
+        finally:
+            db_connection.session.close()

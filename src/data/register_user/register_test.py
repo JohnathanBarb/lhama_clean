2a0c0efc52a16d5fb25cebfd7a9d3078578a6020
Johnathan Barbosa <jbarbosa0504@gmail.com>
2022-11-04 00:33:44 -0300

    feat: creating register user use case and tests

---
+++ src/data/register_user/register_test.py
@@ -0,0 +1,43 @@
+from faker import Faker
+from src.infra.test import UserRepositorySpy
+from .register import RegisterUser
+
+
+faker = Faker()
+
+
+def test_register():
+    """Test registry method"""
+
+    user_repo = UserRepositorySpy()
+    register_user = RegisterUser(user_repo)
+
+    attributes = {"name": faker.name(), "password": faker.name()}
+
+    response = register_user.register(
+        name=attributes["name"], password=attributes["password"]
+    )
+
+    assert user_repo.insert_user_params["name"] == attributes["name"]
+    assert user_repo.insert_user_params["password"] == attributes["password"]
+
+    assert response["success"] is True
+    assert response["data"]
+
+
+def test_fail_register():
+    """Test fail registry method"""
+
+    user_repo = UserRepositorySpy()
+    register_user = RegisterUser(user_repo)
+
+    attributes = {"name": faker.random_number(), "password": faker.name()}
+
+    response = register_user.register(
+        name=attributes["name"], password=attributes["password"]
+    )
+
+    assert not user_repo.insert_user_params
+
+    assert response["success"] is False
+    assert response["data"] is None
